e189fa3c88356fa67263c2f01ceb04cfce1e98cc
Chen, ZhongYi <zhongyi96chen@gmail.com>
2023-01-08 00:21:09 +0100

    fix: eslint fix files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [RouterTestingModule],
+			declarations: [AppComponent]
+		}).compileComponents();
+	});
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	it('should create the app', () => {
+		const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+		const app: AppComponent = fixture.componentInstance;
 
-  it(`should have as title 'pocket-application-web'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pocket-application-web');
-  });
+		expect(app).toBeTruthy();
+	});
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pocket-application-web app is running!');
-  });
+	it(`should have as title 'pocket-application-web'`, () => {
+		const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+		const app: AppComponent = fixture.componentInstance;
+
+		expect(app.title).toEqual('pocket-application-web');
+	});
+
+	it('should render title', () => {
+		const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled: HTMLElement = fixture.nativeElement as HTMLElement;
+
+		expect(compiled.querySelector('.content span')?.textContent).toContain('pocket-application-web app is running!');
+	});
 });
